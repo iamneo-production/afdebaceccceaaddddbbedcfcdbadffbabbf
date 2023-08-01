08f5c06b1b760a491abb5adb80ff6a00abc498f1
Iamneo <techadmin@iamneo.ai>
2023-08-01 09:15:49 +0000

    Backup Commit

--- reactapp/vite.config.js
+++ reactapp/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {port: 8080},
+  server : {port: 8080, host: true, strictPort: true},
 })
 
